1086e067e8503c4cd171c5b0cbe30873941108a3
mhsaiii <132582767+mhsaiii@users.noreply.github.com>
2023-05-04 17:23:07 +0330

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (system.in);
+        System.Out.PrintLn ("choose an operator : +, -, *, or /");
+                Operator input.next().chArt(O);
+                System.out.printLn("enter first number ");
+                number1 = input.nextDuble();
+                System.out.printLn ("enter second number");
+                number2 = input.nextDouble();
+
+
+    }
+
+}
